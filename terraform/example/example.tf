49849db6c0a5a2020f770180738d8e634da4ba9e
Kevin Lim <knlim95@gmail.com>
2024-10-25 17:11:33 -0700

    Add example

---
+++ terraform/example/example.tf
@@ -0,0 +1,21 @@
+module "slackingway-bot" {
+  source = "github.com/kn-lim/slackingway-bot//terraform"
+
+  # Required
+
+  account_id = ""
+  slack_history_channel = ""
+  slack_oauth_token = ""
+
+  # Optional
+
+  # endpoint_timeout = 3
+  # function_name = "slackingway-bot"
+  # region = "us-west-2"
+  # retention_in_days = 3
+  # runtime = "provided.al2023"
+  # tags = {
+  #   App = "slackingway-bot"
+  # }
+  # task_timeout = 3
+}
