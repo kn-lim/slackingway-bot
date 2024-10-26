1a994793eab513d03ff27ff68a52379a93cb04e3
Kevin Lim <knlim95@gmail.com>
2024-10-25 17:11:28 -0700

    Add output

---
+++ terraform/outputs.tf
@@ -0,0 +1,4 @@
+output "api_endpoint" {
+  description = "The endpoint for the API Gateway"
+  value = "https://${aws_api_gateway_rest_api.this.id}.execute-api.${var.region}.amazonaws.com/default/${aws_api_gateway_rest_api.this.name}"
+}
