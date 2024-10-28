aefeeacc629ae2846c99b8f0e55bc7efcee8fff3
Kevin Lim <knlim95@gmail.com>
2024-10-28 16:28:09 -0700

    Fix cycle

--- terraform/cloudwatch.tf
+++ terraform/cloudwatch.tf
@@ -1,11 +1,11 @@
 resource "aws_cloudwatch_log_group" "endpoint" {
-  name              = "/aws/lambda/${aws_lambda_function.endpoint.function_name}"
+  name              = "/aws/lambda/${var.function_name}-endpoint"
   retention_in_days = var.retention_in_days
   tags              = var.tags
 }
 
 resource "aws_cloudwatch_log_group" "task" {
-  name              = "/aws/lambda/${aws_lambda_function.task.function_name}"
+  name              = "/aws/lambda/${var.function_name}-task"
   retention_in_days = var.retention_in_days
   tags              = var.tags
 }
