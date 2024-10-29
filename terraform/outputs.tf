cac0030c50167b5e62ac162d6226f6c3d4f92121
Kevin Lim <knlim95@gmail.com>
2024-10-28 18:26:45 -0700

    Try this

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,4 +1,4 @@
 output "api_endpoint" {
   description = "The endpoint for the API Gateway"
-  value       = aws_api_gateway_deployment.this.invoke_url
+  value       = "${aws_api_gateway_deployment_stage.this.invoke_url}/${aws_api_gateway_resource.this.id}"
 }
