40c6a7e4aa67da4c7fbd973e81146a42c688fc6d
Kevin Lim <knlim95@gmail.com>
2024-10-28 18:10:31 -0700

    Try this

--- terraform/api_gateway.tf
+++ terraform/api_gateway.tf
@@ -30,7 +30,7 @@ resource "aws_api_gateway_deployment" "this" {
   rest_api_id = aws_api_gateway_rest_api.this.id
   stage_name  = "prod"
 
-  depends_on = [aws_api_gateway_method.this]
+  depends_on = [aws_api_gateway_method.this, aws_api_gateway_integration.this]
 
   lifecycle {
     create_before_destroy = true
