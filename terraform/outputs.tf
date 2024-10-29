77fe97738afe743abe70532951c1c22f9de82eee
Kevin Lim <knlim95@gmail.com>
2024-10-28 18:27:33 -0700

    Fix typo

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,4 +1,4 @@
 output "api_endpoint" {
   description = "The endpoint for the API Gateway"
-  value       = "${aws_api_gateway_deployment_stage.this.invoke_url}/${aws_api_gateway_resource.this.id}"
+  value       = "${aws_api_gateway_stage.this.invoke_url}/${aws_api_gateway_resource.this.id}"
 }
