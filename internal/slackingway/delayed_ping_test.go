a9df26a547939cd72b58128cf2349c8f7fd1729f
Kevin Lim <knlim95@gmail.com>
2024-11-03 18:33:19 -0800

    Update tests

--- internal/slackingway/delayed_ping_test.go
+++ internal/slackingway/delayed_ping_test.go
@@ -1,6 +1,7 @@
 package slackingway_test
 
 import (
+	"errors"
 	"net/http"
 	"testing"
 	"time"
@@ -18,18 +19,35 @@ func TestDelayedPing(t *testing.T) {
 	ctrl := gomock.NewController(t)
 	defer ctrl.Finish()
 
-	// Create a new MockSlackingway object
-	mockSlackingway := NewMockSlackingway(ctrl)
-	mockSlackingway.EXPECT().NewResponse(gomock.Any()).Return(&http.Request{}, nil)
-	mockSlackingway.EXPECT().SendResponse(gomock.Any()).Return(nil)
+	t.Run("Error on NewResponse", func(t *testing.T) {
+		mockSlackingway := NewMockSlackingway(ctrl)
+		mockSlackingway.EXPECT().NewResponse(gomock.Any()).Return(&http.Request{}, errors.New("error!!!"))
 
-	// Set the PingDelay to 1 millisecond for tests
-	slackingway.PingDelay = 1 * time.Millisecond
+		_, err := slackingway.DelayedPing(mockSlackingway)
+		assert.NotNil(t, err)
+	})
 
-	// Run tests
-	expected := slack.Msg{Text: "Delayed Pong!", ReplaceOriginal: true}
-	actual, err := slackingway.DelayedPing(mockSlackingway)
+	t.Run("Error on SendResponse", func(t *testing.T) {
+		mockSlackingway := NewMockSlackingway(ctrl)
+		mockSlackingway.EXPECT().NewResponse(gomock.Any()).Return(&http.Request{}, nil)
+		mockSlackingway.EXPECT().SendResponse(gomock.Any()).Return(errors.New("error!!!"))
 
-	assert.Nil(t, err)
-	assert.Equal(t, expected, actual)
+		_, err := slackingway.DelayedPing(mockSlackingway)
+		assert.NotNil(t, err)
+	})
+
+	t.Run("Successful DelayedPing", func(t *testing.T) {
+		mockSlackingway := NewMockSlackingway(ctrl)
+		mockSlackingway.EXPECT().NewResponse(gomock.Any()).Return(&http.Request{}, nil)
+		mockSlackingway.EXPECT().SendResponse(gomock.Any()).Return(nil)
+
+		// Set PingDelay to a minimal value for testing
+		slackingway.PingDelay = 1 * time.Millisecond
+
+		expected := slack.Msg{Text: "Delayed Pong!", ReplaceOriginal: true}
+		actual, err := slackingway.DelayedPing(mockSlackingway)
+
+		assert.Nil(t, err)
+		assert.Equal(t, expected, actual)
+	})
 }
