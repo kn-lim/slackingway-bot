84a444f1c0a9f5fda867aadad5550ec6c6849e93
Kevin Lim <knlim95@gmail.com>
2024-11-08 20:26:39 -0800

    Add arn outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,3 +2,13 @@ output "api_endpoint" {
   description = "The endpoint for the API Gateway"
   value       = "${aws_api_gateway_stage.this.invoke_url}/${aws_api_gateway_resource.this.path_part}"
 }
+
+output "endpoint_function_arn" {
+  description = "The ARN of the Endpoint Lambda function"
+  value       = "${aws_lambda_function.endpoint.arn}"
+}
+
+output "task_function_arn" {
+  description = "The ARN of the Task Lambda function"
+  value       = "${aws_lambda_function.task.arn}"
+}
