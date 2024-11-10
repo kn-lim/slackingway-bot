cb6b787b92b31163e9a3dd2b0fb6a7607a167716
Kevin Lim <knlim95@gmail.com>
2024-11-09 22:39:50 -0800

    Add test

--- internal/slackingway/menu_test.go
+++ internal/slackingway/menu_test.go
@@ -43,6 +43,52 @@ func TestMenu(t *testing.T) {
 	})
 }
 
+func TestReceivedMenu(t *testing.T) {
+	// Create a new mock controller
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	testView := slack.View{
+		State: &slack.ViewState{
+			Values: map[string]map[string]slack.BlockAction{},
+		},
+	}
+
+	t.Run("Error on GetUserInfo", func(t *testing.T) {
+		MockSlackAPIClient := NewMockSlackAPIClient(ctrl)
+		MockSlackAPIClient.EXPECT().GetUserInfo(gomock.Any()).Return(&slack.User{}, errors.New("error!!!"))
+
+		s := &slackingway.SlackingwayWrapper{
+			APIClient: MockSlackAPIClient,
+			SlackRequestBody: &slackingway.SlackRequestBody{
+				UserID: "definitely_a_valid_user_id",
+				View:   testView,
+			},
+		}
+		msg, err := slackingway.ReceivedMenu(s)
+
+		assert.NotNil(t, err)
+		assert.Empty(t, msg)
+	})
+
+	t.Run("Successful ReceivedMenu", func(t *testing.T) {
+		MockSlackAPIClient := NewMockSlackAPIClient(ctrl)
+		MockSlackAPIClient.EXPECT().GetUserInfo(gomock.Any()).Return(&slack.User{RealName: "DefinitelyA RealName"}, nil)
+
+		s := &slackingway.SlackingwayWrapper{
+			APIClient: MockSlackAPIClient,
+			SlackRequestBody: &slackingway.SlackRequestBody{
+				UserID: "definitely_a_valid_user_id",
+				View:   testView,
+			},
+		}
+		msg, err := slackingway.ReceivedMenu(s)
+
+		assert.Nil(t, err)
+		assert.NotEmpty(t, msg)
+	})
+}
+
 func TestCreateMenuModal(t *testing.T) {
 	modal := slackingway.CreateMenuModal()
 	assert.NotNil(t, modal)
