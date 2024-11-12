8c195f61e0d935d1feb00974d066b9e861f50725
Kevin Lim <knlim95@gmail.com>
2024-11-11 20:48:46 -0800

    Add test

--- internal/dice/roll_test.go
+++ internal/dice/roll_test.go
@@ -1,11 +1,19 @@
 package dice_test
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/kn-lim/slackingway-bot/internal/dice"
+)
 
 func TestRoll(t *testing.T) {
-	t.Skip("Not implemented")
-}
+	testInput := "2d6+3"
 
-func TestEvaluateModifiers(t *testing.T) {
-	t.Skip("Not implemented")
+	resultString, resultInt, err := dice.Roll(testInput)
+
+	assert.Nil(t, err)
+	assert.NotEmpty(t, resultString)
+	assert.NotZero(t, resultInt)
 }
